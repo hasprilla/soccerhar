9fe4b5f7e24f67ae7a947e55ea307dfd1ed16e0b
Harvey Asprilla <kikiyam_8@hotmail.com>
2023-06-03 18:37:45 -0500

    Implementacion custom widgets listview horizontal

---
+++ lib/presentation/screens/home/widgets/custom_listview_horizontal.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:soccerhar/presentation/providers/teams/teams_providers.dart';
+
+import '../../../../domain/entities/league.dart';
+
+class CustomListviewHorizontal extends ConsumerWidget {
+  final List<League> leagues;
+  const CustomListviewHorizontal({
+    super.key,
+    required this.leagues,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return SizedBox(
+      height: 50,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: leagues.length,
+          itemBuilder: (context, index) {
+            final league = leagues[index];
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 3),
+              child: FilledButton.tonal(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(
+                    index == 0 ? Colors.amber : Colors.blue,
+                  ),
+                ),
+                onPressed: () => ref
+                    .read(teamsNotifierProvider.notifier)
+                    .loadPage(league.strLeague.replaceAll(' ', '%20')),
+                child: Text(league.strLeague),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
