372bbaf4e53cfefead73d58d1e8f2cf0e997d5da
Harvey Asprilla <kikiyam_8@hotmail.com>
2023-06-03 14:13:58 -0500

    ADD: flutter riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() => runApp(MyApp());
+import 'config/constants/environment.dart';
+import 'config/router/app_router.dart';
+
+void main() => runApp(const ProviderScope(child: MyApp()));
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      title: Environment.appName,
+      routerConfig: router,
     );
   }
 }
