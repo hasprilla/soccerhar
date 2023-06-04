694c2198c6d73bff3c72a2793d8c567aaf1e06c1
Harvey Asprilla <kikiyam_8@hotmail.com>
2023-06-03 23:13:38 -0500

    ADD: ruta de detail

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,4 +1,5 @@
 import 'package:go_router/go_router.dart';
+import 'package:soccerhar/presentation/screens/detail/detail_screen.dart';
 import 'package:soccerhar/presentation/screens/home/home_screens.dart';
 
 final router = GoRouter(
@@ -8,6 +9,16 @@ final router = GoRouter(
       path: '/',
       name: 'home',
       builder: (context, state) => const HomeScreen(),
-    )
+    ),
+    GoRoute(
+      path: '/detail/:id',
+      name: 'detail',
+      builder: (context, state) {
+        final id = state.params['id'] ?? 'no-id';
+        return DetailScreen(
+          id: id,
+        );
+      },
+    ),
   ],
 );
